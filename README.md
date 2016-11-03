a0b4e001f3acc761fd0fe13db871455357fdba25
Lars Kellogg-Stedman <lars@redhat.com>
2016-11-03 12:37:03 -0400

    update to use cliff for cli handling

--- README.md
+++ README.md
@@ -1,15 +1,7 @@
 Health checks for systemd services, for use with [oschecks][].
 
-    Usage: oschecks systemd [OPTIONS] COMMAND [ARGS]...
-
-      Health checks for systemd units.
-
-    Options:
-      --help  Show this message and exit.
-
-    Commands:
-      check_unit_active   Check if the named unit is active.
-      check_unit_enabled  Check if the named unit is enabled.
-      check_unit_exists   Check if the named unit exists.
+  systemd unit active
+  systemd unit enabled
+  systemd unit exists
 
 [oschecks]: https://github.com/larsks/oschecks
--- oschecks_systemd/check_systemd.py
+++ oschecks_systemd/check_systemd.py
@@ -1,69 +1,97 @@
 import dbus
-import click
 from oschecks_systemd.systemd import Systemd
 import oschecks.common as common
 
-@click.group('systemd')
-@click.pass_context
-def cli(ctx):
-    '''Health checks for systemd units.'''
 
-    ctx.obj = Systemd()
+class SystemdCommand (common.CheckCommand):
+    def take_action(self, parsed_args):
+        self.systemd = Systemd()
 
 
-@cli.command()
-@click.argument('unit')
-@click.pass_context
-def check_unit_active(ctx, unit):
-    '''Check if the named unit is active.'''
-    try:
-        res = ctx.obj.get_unit(unit)
-        state = res.get('ActiveState')
-    except dbus.exceptions.DBusException:
-        raise common.ExitCritical('Failed to query unit {} status'.format(
-            unit))
+class CheckUnitActive(SystemdCommand):
+    def get_parser(self, prog_name):
+        p = super(CheckUnitActive, self).get_parser(prog_name)
+        p.add_argument('unit_name')
+        return p
 
-    if state == 'failed':
-        raise common.ExitCritical('Unit {} has failed'.format(unit))
-    elif state == 'inactive':
-        raise common.ExitWarning('Unit {} is inactive'.format(unit))
-    elif state == 'active':
-        raise common.ExitOkay('Unit {} is active'.format(unit))
-    else:
-        raise common.ExitWTF('Unit {} is in an unknown state'.format(unit))
+    def take_action(self, parsed_args):
+        '''Check if the named unit is active.'''
 
+        super(CheckUnitActive, self).take_action(parsed_args)
 
-@cli.command()
-@click.argument('unit')
-@click.pass_context
-def check_unit_enabled(ctx, unit):
-    '''Check if the named unit is enabled.'''
+        try:
+            res = self.systemd.get_unit(parsed_args.unit_name)
+            state = res.get('ActiveState')
+        except dbus.exceptions.DBusException:
+            return (common.RET_CRIT,
+                    'Failed to query unit {} status'.format(
+                        parsed_args.unit_name))
 
-    try:
-        state = ctx.obj.get_unit_file(unit)
-    except dbus.exceptions.DBusException:
-        raise common.ExitCritical('Failed to query unit {} status'.format(
-            unit))
+        if state == 'failed':
+            exitcode = common.RET_CRIT
+            msg = 'Unit {} has failed'.format(parsed_args.unit_name)
+        elif state == 'inactive':
+            exitcode = common.RET_WARN
+            msg = 'Unit {} is inactive'.format(parsed_args.unit_name)
+        elif state == 'active':
+            exitcode = common.RET_OKAY
+            msg = 'Unit {} is active'.format(parsed_args.unit_name)
+        else:
+            exitcode = common.RET_WTF
+            msg = 'Unit {} is in an unknown state'.format(parsed_args.unit_name)
 
-    if state in ['enabled']:
-        raise common.ExitOkay('Unit {} is enabled'.format(unit))
-    elif state in ['enabled-runtime']:
-        raise common.ExitWarning('Unit {} is enabled temporarily'.format(unit))
-    else:
-        raise common.ExitCritical('Unit {} is not enabled'.format(unit))
+        return (exitcode, msg)
 
 
-@cli.command()
-@click.argument('unit')
-@click.pass_context
-def check_unit_exists(ctx, unit):
-    '''Check if the named unit exists.'''
+class CheckUnitEnabled(SystemdCommand):
+    def get_parser(self, prog_name):
+        p = super(CheckUnitEnabled, self).get_parser(prog_name)
+        p.add_argument('unit_name')
+        return p
 
-    try:
-        state = ctx.obj.get_unit_file(unit)
-    except dbus.exceptions.DBusException:
-        raise common.ExitCritical('Failed to query unit {} status'.format(
-            unit))
+    def take_action(self, parsed_args):
+        '''Check if the named unit is active.'''
 
-    raise common.ExitOkay('Unit {} exists'.format(unit))
+        super(CheckUnitEnabled, self).take_action(parsed_args)
 
+        try:
+            state = self.systemd.get_unit_file(parsed_args.unit_name)
+        except dbus.exceptions.DBusException:
+            return (common.RET_CRIT,
+                    'Failed to query unit {} status'.format(
+                        parsed_args.unit_name))
+
+        if state == 'enabled':
+            exitcode = common.RET_OKAY
+            msg = 'Unit {} is enabled'.format(parsed_args.unit_name)
+        elif state == 'enabled-runtime':
+            exitcode = common.RET_WARN
+            msg = 'Unit {} is enabled temporarily'.format(parsed_args.unit_name)
+        else:
+            exitcode = common.RET_CRIT
+            msg = 'Unit {} is not enabled'.format(parsed_args.unit_name)
+
+        return (exitcode, msg)
+
+
+class CheckUnitExists(SystemdCommand):
+    def get_parser(self, prog_name):
+        p = super(CheckUnitExists, self).get_parser(prog_name)
+        p.add_argument('unit_name')
+        return p
+
+    def take_action(self, parsed_args):
+        '''Check if the named unit exists.'''
+
+        super(CheckUnitExists, self).take_action(parsed_args)
+
+        try:
+            state = self.systemd.get_unit_file(parsed_args.unit_name)
+        except dbus.exceptions.DBusException:
+            return (common.RET_CRIT,
+                    'Failed to query unit {} status'.format(
+                        parsed_args.unit_name))
+
+        msg = 'Unit {} exists'.format(parsed_args.unit_name)
+
+        return (common.RET_OKAY, msg)
